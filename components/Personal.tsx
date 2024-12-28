aace18c16ede7abe11e07695935fe00b2ea7ac6c
Mertgencc <merttgencc61@gmail.com>
2024-12-28 12:15:12 +0300

    yazılar düzeltildi

--- components/Personal.tsx
+++ components/Personal.tsx
@@ -5,7 +5,7 @@ export default function Personal() {
   return (
     
     <div className="flex space-x-24">
-      <Link href="/" className="text-xl text-[#999] pt-2 mb-2 ml-2">
+      <Link href="/" className="text-xl text-[#999] pt-2 mb-2 ml-2 lg:flex">
         Mert GENÇ
       </Link>
       <h2 className="text-xl text-[#999] pt-2 mb-2 ml-2 hidden lg:flex">
